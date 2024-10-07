0581efbce25e7c7e97c5be573895ca4ddbdea6ca
KimWooJun <wojun74@naver.com>
2024-10-07 17:27:48 +0900

    First

---
+++ src/main/java/com/green/day01/ch01/Ch01Misson02.java
@@ -0,0 +1,28 @@
+package com.green.day01.ch01;
+
+public class Ch01Misson02 {
+    public static void main(String[] args) {
+        //아래 작성한 내용을 예상
+
+        System.out.println("2 + 5 = " + 2 + 5);
+        System.out.println("2 + 5 = " + 5);
+//        "2 + 5 = " + 2 먼저 연산이 된다. 타입이 다르다
+ //       "2 + 5 = " + " 2 "
+ //       "2 + 5 = 2 "
+        System.out.println("2 + 5 = 25");
+        //"2 + 5 = 2" + 5  두 번째 연산이 된다. 타입이 다르다
+        //"2 + 5 = 2" + "5"
+        //"2 + 5 = 25"
+
+        System.out.println("2 + 5 = " + (2 + 5));
+        System.out.println("2 + 5 = " + 7);
+        //(2 + 5) 연산의 우선순위가 더 높기 때문에 먼저 연산이 된다
+        //7
+
+        System.out.println("2 + 5 = 7");
+        //"2 + 5 = " + 7 두 번째 연산이 된다 타입이 다르다
+        //"2 + 5 = " + "7"
+        //"2 + 5 = 7"
+
+    }
+}
---
+++ src/main/java/com/green/day01/ch02/Constants.java
@@ -0,0 +1,16 @@
+package com.green.day01.ch02;
+
+public class Constants {
+    public static void main(String[] args) {
+        final int MAX_SIZE = 100;
+        final char CONST_CHAR = '상';
+        final int CONST_ASSIGNED;
+
+        CONST_ASSIGNED = 12;
+
+        System.out.printf("상수 1 : %d\n", MAX_SIZE);
+        System.out.printf("상수 2 : %c\n", CONST_CHAR);
+        System.out.printf("상수 3 : %d\n", CONST_ASSIGNED);
+
+    }
+}
